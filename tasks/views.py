24687a4046404a7093ac16a0d8bba863e718f0d8
Jordy-Pedro-Valencia-Jara <jquispejar@unsa.edu.pe>
2025-04-28 18:49:57 -0500

    paginacion

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from rest_framework.pagination import PageNumberPagination
 from rest_framework import viewsets
 from .serializer import TaskSerializer
 from .models import Task
 
+class TaskPagination(PageNumberPagination):
+    page_size=16
+
+
 class TaskView(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
-    queryset = Task.objects.all()
+    queryset = Task.objects.all().order_by('-id')
+    pagination_class=TaskPagination
