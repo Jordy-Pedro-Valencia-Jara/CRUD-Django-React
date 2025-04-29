d12ad34e103afef653e8727b1d9533d0fd8f353d
Jordy-Pedro-Valencia-Jara <jquispejar@unsa.edu.pe>
2025-04-28 19:04:14 -0500

    paginacion front

--- client/src/api/tasks.api.ts
+++ client/src/api/tasks.api.ts
@@ -2,7 +2,9 @@ import axios from "axios";
 const apiURL=axios.create({
     baseURL:'https://crud-django-react-1il6.onrender.com/tasks/api/v1/tasks/',
 })
-export const getAllTasks=()=>apiURL.get('/');
+export const getAllTasks=(page:number=1)=>{
+    return apiURL.get(`/?page=${page}`)
+}
 export const getTask=(id)=>apiURL.get(`/${id}/`);
 export const addTask=(tasks)=>apiURL.post('/',tasks);
 export const DeleteTask=(id)=>apiURL.delete(`/${id}/`);
