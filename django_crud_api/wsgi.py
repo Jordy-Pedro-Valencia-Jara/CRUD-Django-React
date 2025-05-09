441071078ed387370ea86a4b177f9c25c104101d
Jordy-Pedro-Valencia-Jara <jquispejar@unsa.edu.pe>
2025-05-09 12:16:41 -0500

    definiendo BASE_DIR

--- django_crud_api/wsgi.py
+++ django_crud_api/wsgi.py
@@ -12,6 +12,6 @@ from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_crud_api.settings')
-
+BASE_DIR = Path(__file__).resolve().parent.parent
 application = get_wsgi_application()
 application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
