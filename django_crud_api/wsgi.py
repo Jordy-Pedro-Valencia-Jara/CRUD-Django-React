6f7da949a04b259755df1a272fe2c74eb12a64c6
Jordy-Pedro-Valencia-Jara <jquispejar@unsa.edu.pe>
2025-05-09 12:19:34 -0500

    importando path

--- django_crud_api/wsgi.py
+++ django_crud_api/wsgi.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+from pathlib import Path
 from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 
